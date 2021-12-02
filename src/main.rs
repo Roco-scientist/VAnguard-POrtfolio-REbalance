6422d487e375a9d4f526d10e35d1ef9a068b986a
Rory Coffey <coffeyrt@gmail.com>
2021-12-01 20:11:54 -0500

    further adding anyhow error handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use alpaca_finance::{Account, Alpaca};
+use anyhow::Result;
 use chrono::Local;
 use std::{fs::File, io::Write};
 use vapore::arguments;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let args = arguments::Args::new();
-    let key_id = std::env::var("APCA_API_KEY_ID").unwrap_or_else(|_| "None".to_string());
-    let key = std::env::var("APCA_API_SECRET_KEY").unwrap_or_else(|_| "None".to_string());
+    let key_id = std::env::var("APCA_API_KEY_ID").unwrap_or_else(|_| String::new());
+    let key = std::env::var("APCA_API_SECRET_KEY").unwrap_or_else(|_| String::new());
     let additional_us_stock: f32;
-    if key_id != *"None" && key != *"None" {
-        let alpaca = Alpaca::live(&key_id, &key).await.unwrap();
-        let account = Account::get(&alpaca).await.unwrap();
+    if !key_id.is_empty() && !key.is_empty() {
+        let alpaca = Alpaca::live(&key_id, &key).await?;
+        let account = Account::get(&alpaca).await?;
         additional_us_stock = account.equity as f32;
     } else {
         additional_us_stock = 0.0
     }
-    let vanguard_holdings = vapore::holdings::parse_csv_download(&args.csv_path, args.clone())
-        .unwrap_or_else(|err| panic!("Holdings error: {}", err));
+    let vanguard_holdings = vapore::holdings::parse_csv_download(&args.csv_path, args.clone())?;
+    //    .unwrap_or_else(|err| panic!("Holdings error: {}", err));
     let rebalance = vapore::calc::to_buy(vanguard_holdings, additional_us_stock, args.clone());
     println!(
         "DESCRIPTIONS:\n{}\n\n{}",
@@ -27,7 +28,7 @@ async fn main() {
     if args.output {
         let datetime = Local::now().format("%Y-%m-%d_%H:%M");
         let outfile = format!("{}_vanguard_rebalance.txt", datetime);
-        let mut file = File::create(outfile).unwrap();
+        let mut file = File::create(outfile)?;
         file.write_all(
             format!(
                 "DESCRIPTIONS:\n{}\n\n{}",
@@ -35,7 +36,7 @@ async fn main() {
                 rebalance
             )
             .as_bytes(),
-        )
-        .unwrap()
+        )?
     }
+    Ok(())
 }
